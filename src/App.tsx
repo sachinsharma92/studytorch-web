77b199819f06b99345a8e82484eaa8039ac5af02
Aakash Yadav <aakash@mywoobly.com>
2021-12-01 11:51:48 +0530

    INITIAL commit

--- src/App.tsx
+++ src/App.tsx
@@ -21,30 +21,3 @@ function App() {
 }
 
 export default App;
-
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Kernify React Template
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://www.kernify.com"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Kernify Creation Pvt. Limited
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
