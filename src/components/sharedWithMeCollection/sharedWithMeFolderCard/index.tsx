4e029d7420024a42e5d98d87e7a457f58938aea7
Sachin Sharma <sachin@gryt.fit>
2022-01-06 01:05:27 +0530

    ADD: drawer card

--- src/components/sharedWithMeCollection/sharedWithMeFolderCard/index.tsx
+++ src/components/sharedWithMeCollection/sharedWithMeFolderCard/index.tsx
@@ -1,49 +1,48 @@
 import { Card } from 'antd';
 import image from "../../../assets/images/sharedWithMe/image.svg";
 import EllipsisMenu from "../../ellipsisMenu"
-import CollectionFolder from "./collectionFolderSVG"
-import Images from "../../../assets/images/images";
+import CollectionFolder from "../../../common/collectionFolderSVG"
 
 import './styles.scss';
 
 export interface SharedFolderCardProps {
-    folderName: string,
-    folderColor: string,
-    notes: string,
-    quizzes: string,
-    sharedBy: string
+  folderName: string,
+  folderColor: string,
+  notes: string,
+  quizzes: string,
+  sharedBy: string,
 }
 
 function SharedFolderCard(props: SharedFolderCardProps) {
-    return(
-        <div className="shared-folder-card-style">
-            <Card>
-                <div className="top-card-layer">
-                    <div className="top-card-layer-left ">
-                        <div className="folder-icon">
-                            <CollectionFolder fillColor={props.folderColor}/>
-                        </div>
-                        <div className="collection-details">
-                            <div className="collection-name">
-                                <p>{props.folderName}</p>
-                            </div>
-                            <div className="collection-notes-quizzes">
-                                <p>{props.notes} notes, {props.quizzes} quizzes</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="top-card-layer-right">
-                        <EllipsisMenu menuItems={[{name: "Delete", iconName: "deleteIcon"}, {name: "More info", iconName: "infoIcon"}]} />
-                    </div>
-                </div>
-                <hr className="line" />
-                <div className="bottom-card-layer">
-                    <img src={image} />
-                    <p>Shared by {props.sharedBy}</p>
-                </div>
-            </Card>        
+  return (
+    <div className="shared-folder-card-style">
+      <Card>
+        <div className="top-card-layer">
+          <div className="top-card-layer-left ">
+            <div className="folder-icon">
+              <CollectionFolder fillColor={props.folderColor} />
+            </div>
+            <div className="collection-details">
+              <div className="collection-name">
+                <p>{props.folderName}</p>
+              </div>
+              <div className="collection-notes-quizzes">
+                <p>{props.notes} notes, {props.quizzes} quizzes</p>
+              </div>
+            </div>
+          </div>
+          <div className="top-card-layer-right">
+            <EllipsisMenu menuItems={[{ name: "View Details", iconName: "infoIcon", }, { name: "Remove from shared", iconName: "deleteIcon" }]} />
+          </div>
         </div>
-    )
+        <hr className="line" />
+        <div className="bottom-card-layer">
+          <img src={image} />
+          <p>Shared by {props.sharedBy}</p>
+        </div>
+      </Card>
+    </div>
+  )
 }
 
 export default SharedFolderCard;
--- src/pages/sharedWithMe/sharedWithMeScreen.tsx
+++ src/pages/sharedWithMe/sharedWithMeScreen.tsx
@@ -1,104 +1,186 @@
+import { useState } from "react";
+import { Drawer, Button } from 'antd';
 import PrimaryLayout from "../../common/primaryLayout/primaryLayout"
 import SharedWithMeCollection from "../../components/sharedWithMeCollection"
+import CollectionFolderSVG from "../../common/collectionFolderSVG";
+
 // Styles
 import './styles.scss';
 
 function SharedWithMeScreen() {
-    const folderList = [
+  const folderList = [
+    {
+      time: "Today",
+      folders: [
         {
-            time: "Today",
-            folders: [
-                {
-                    folderName: "Maths",
-                    folderColor: "#6455CD",
-                    notes: "20",
-                    quizzes: "5",
-                    sharedBy: "Sukirti"
-                },
-                {
-                    folderName: "Physics",
-                    folderColor: "#FCAB8E",
-                    notes: "0",
-                    quizzes: "5",
-                    sharedBy: "Sukirti"
-                },
-                {
-                    folderName: "Chem",
-                    folderColor: "#FF8B8B",
-                    notes: "20",
-                    quizzes: "5",
-                    sharedBy: "Sukirti"
-                },
-                {
-                    folderName: "Bio",
-                    folderColor: "#6FBEF6",
-                    notes: "20",
-                    quizzes: "5",
-                    sharedBy: "Sukirti"
-                }
-            ] 
+          folderName: "Maths",
+          folderColor: "#6455CD",
+          notes: "20",
+          quizzes: "5",
+          sharedBy: "Aakash"
         },
         {
-            time: "Yesterday",
-            folders: [
-                {
-                    folderName: "English",
-                    folderColor: "#6455CD",
-                    notes: "20",
-                    quizzes: "5",
-                    sharedBy: "Sukirti"
-                },
-                {
-                    folderName: "SST",
-                    folderColor: "#6FBEF6",
-                    notes: "20",
-                    quizzes: "5",
-                    sharedBy: "Sukirti"
-                },
-                {
-                    folderName: "Bio",
-                    folderColor: "#FF8B8B",
-                    notes: "20",
-                    quizzes: "5",
-                    sharedBy: "Sukirti"
-                },
-                {
-                    folderName: "English",
-                    folderColor: "#6455CD",
-                    notes: "20",
-                    quizzes: "5",
-                    sharedBy: "Sukirti"
-                },
-                {
-                    folderName: "SST",
-                    folderColor: "#6455CD",
-                    notes: "20",
-                    quizzes: "5",
-                    sharedBy: "Sukirti"
-                },
-            ]
+          folderName: "Physics",
+          folderColor: "#FCAB8E",
+          notes: "0",
+          quizzes: "5",
+          sharedBy: "Aakash"
+        },
+        {
+          folderName: "Chem",
+          folderColor: "#FF8B8B",
+          notes: "20",
+          quizzes: "5",
+          sharedBy: "Aakash"
+        },
+        {
+          folderName: "Bio",
+          folderColor: "#6FBEF6",
+          notes: "20",
+          quizzes: "5",
+          sharedBy: "Aakash"
         }
-        
-    ]
+      ]
+    },
+    {
+      time: "Yesterday",
+      folders: [
+        {
+          folderName: "English",
+          folderColor: "#6455CD",
+          notes: "20",
+          quizzes: "5",
+          sharedBy: "Sachin Sharma"
+        },
+        {
+          folderName: "SST",
+          folderColor: "#6FBEF6",
+          notes: "20",
+          quizzes: "5",
+          sharedBy: "Sachin Sharma"
+        },
+        {
+          folderName: "Bio",
+          folderColor: "#FF8B8B",
+          notes: "20",
+          quizzes: "5",
+          sharedBy: "Sachin Sharma"
+        },
+        {
+          folderName: "English",
+          folderColor: "#6455CD",
+          notes: "20",
+          quizzes: "5",
+          sharedBy: "Sachin Sharma"
+        },
+        {
+          folderName: "SST",
+          folderColor: "#6455CD",
+          notes: "20",
+          quizzes: "5",
+          sharedBy: "Sachin Sharma"
+        },
+      ]
+    },
+    {
+      time: "Last Week",
+      folders: [
+        {
+          folderName: "English",
+          folderColor: "#6455CD",
+          notes: "20",
+          quizzes: "5",
+          sharedBy: "Sukirti"
+        },
+        {
+          folderName: "SST",
+          folderColor: "#6FBEF6",
+          notes: "20",
+          quizzes: "5",
+          sharedBy: "Sukirti"
+        },
+        {
+          folderName: "Bio",
+          folderColor: "#FF8B8B",
+          notes: "20",
+          quizzes: "5",
+          sharedBy: "Sukirti"
+        },
+        {
+          folderName: "English",
+          folderColor: "#6455CD",
+          notes: "20",
+          quizzes: "5",
+          sharedBy: "Sukirti"
+        },
+        {
+          folderName: "SST",
+          folderColor: "#6455CD",
+          notes: "20",
+          quizzes: "5",
+          sharedBy: "Sukirti"
+        },
+      ]
+    }
+  ]
 
-    return(
-        <>
-            <PrimaryLayout>
-                <div className="shared-page-style">
-                    <h3 className="title3">My Collection</h3>
-                    {
-                        folderList.map((data, index) => (
-                            <SharedWithMeCollection 
-                                key={index}
-                                timeFilter={data.time}
-                                folders={data.folders}
-                            />
-                        ))
-                    }
-                </div>
-            </PrimaryLayout>            
-        </>        
-    )
+  const [visible, setVisible] = useState(false);
+  const handleToggleDrawer = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <>
+      <PrimaryLayout>
+        <div className="shared-page-style">
+          <h3 className="title3">Shared with me</h3>
+          {
+            folderList.map((data, index) => (
+              <SharedWithMeCollection
+                key={index}
+                timeFilter={data.time}
+                folders={data.folders}
+              />
+            ))
+          }
+        </div>
+
+
+        {/* Drawer Style here */}
+        <Drawer
+          title="Shared Information"
+          maskClosable={true}
+          closable={false}
+          className="shared-information-drawer"
+          placement="right"
+          onClose={handleToggleDrawer}
+          visible={true}
+        >
+          <div className="detail-section">
+            <div className="flex">
+              <div className="folder-icon">
+                <CollectionFolderSVG fillColor={"#6C5ECF"} />
+              </div>
+              <div className="info-sec">
+                <h4 className="title4">Maths</h4>
+                <p>20 Notes, 2 quizes</p>
+              </div>
+            </div>
+            <div className="shared-details">
+              <div className="name">
+                <p>Shared by </p>
+                <h4 className="title4">Ayush Parashar</h4>
+              </div>
+              <div className="date">
+                <p>Date</p>
+                <h4 className="title4">05/08/2021</h4>
+              </div>
+            </div>
+          </div>
+        </Drawer>
+      </PrimaryLayout>
+    </>
+  )
 }
 
 export default SharedWithMeScreen;
